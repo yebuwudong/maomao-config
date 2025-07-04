630fcb9a71bdf0dbd34ba939e09040c2b7c5a446
DreamMaoMao <2523610504@qq.com>
2025-07-04 18:22:43 +0800

    u
    u
    
    u
    
    u
    
    u
    
    u

--- autostart.sh
+++ autostart.sh
@@ -1,59 +1,53 @@
-#! /bin/bash
+ #!/bin/bash
 # 自启动脚本 仅作参考
 
 set +e
 
 # obs
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
-/usr/lib/xdg-desktop-portal-wlr &
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots >/dev/null 2>&1
+/usr/lib/xdg-desktop-portal-wlr >/dev/null 2>&1 &
 
 # notify
-swaync &
+swaync -c ~/.config/maomao/swaync/config.json -s ~/.config/maomao/swaync/style.css >/dev/null 2>&1 &
 
 # night light
-wlsunset -T 3501 -t 3500 &
+wlsunset -T 3501 -t 3500 >/dev/null 2>&1 &
 
 # wallpaper
-swaybg -i ~/.config/maomao/wallpaper/czd.png &
+swaybg -i ~/.config/maomao/wallpaper/czd.png >/dev/null 2>&1 &
 
 # top bar
-waybar -c ~/.config/maomao/waybar/config -s ~/.config/maomao/waybar/style.css &
+waybar -c ~/.config/maomao/waybar/config -s ~/.config/maomao/waybar/style.css >/dev/null 2>&1 &
 
 # dock
-lavalauncher -c ~/.config/maomao/lavalauncher/lavalauncher.conf &
-# lxqt-panel 2> /dev/null &
+lavalauncher -c ~/.config/maomao/lavalauncher/lavalauncher.conf >/dev/null 2>&1 &
 
 # xwayland dpi scale
-# echo "Xft.dpi: 140" | xrdb -merge #dpi缩放
-xrdb merge ~/.Xresources
+echo "Xft.dpi: 140" | xrdb -merge #dpi缩放
 
 # ime input
-fcitx5 --replace -d 2> /dev/null &
+fcitx5 --replace -d >/dev/null 2>&1 &
 
 # keep clipboard content
-wl-clip-persist --clipboard regular --reconnect-tries 0 &
+wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
 
 # clipboard content manager
-wl-paste --type text --watch cliphist store & 
+wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
 
 # bluetooth 
-blueman-applet &
+blueman-applet >/dev/null 2>&1 &
 
 # network
-nm-applet &
+nm-applet >/dev/null 2>&1 &
 
 # Permission authentication
-/usr/lib/xfce-polkit/xfce-polkit &
-
-# for private use not for you
-[ -e /dev/sda4 ] && udisksctl mount -t ext4 -b /dev/sda4
+/usr/lib/xfce-polkit/xfce-polkit >/dev/null 2>&1 &
 
 # idle to lightdown and shutdown screen
-~/.config/maomao/scripts/idle.sh &
+~/.config/maomao/scripts/idle.sh >/dev/null 2>&1 &
 
 # inhibit by audio
-sway-audio-idle-inhibit &
+sway-audio-idle-inhibit >/dev/null 2>&1 &
 
 # change light value and volume value by swayosd-client in keybind
-swayosd-server &
-
+swayosd-server >/dev/null 2>&1 &
